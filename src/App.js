4ae79c4448d94a407fd7fa31cfe99ed447b5a1fe
a-poor <a-poor@users.noreply.github.com>
2021-12-24 19:38:11 -0800

    Removing antd

--- src/App.js
+++ src/App.js
@@ -1,78 +1,83 @@
-import { Layout, Menu, Space, Typography, Card, Col, Row, Image } from 'antd';
-
 import './App.css';
-import 'antd/dist/antd.css';
 
-const { Title, Text } = Typography;
-const { Header, Content, Footer } = Layout;
-
-function range(n) {
-  return new Array(n).fill(null).map((_, i) => i);
+function Header({  }) {
+  return <>
+  </>
 }
 
-function Body() {
-  const data = [
-    "data 1",
-    "data 2",
-    "data 3",
-    "data 4",
-    "data 5",
-    "data 6",
-    "data 7",
-    "data 8",
-    "data 9",
-  ];
+function CellHeader({ idx }) {
+  return <> 
+    <div>
+      <p>Frame: {idx}</p>
+    </div>
+  </>
+}
 
-  return <>
-    <Space direction="vertical" style={{ width: "100%" }}>
-      {
-        range(3).map(i => (
-          <Row key={i} gutter={16}>
-            {
-              range(3).map(j => (
-                <Col span={8}>
-                  <Card 
-                    hoverable
-                    cover={<Image />}
-                    key={j}
-                    title={`Card ${i}, ${i}`}
-                  >
-                    <p>Row: {i}</p>
-                    <p>Col: {j}</p>
-                  </Card>
-                </Col>
-              ))
-            }
-          </Row>
-        ))
-      }
-    </Space>
+function CellImage({  }) {
+  return <> 
+    <div>
+    </div>
+  </>
+}
+
+function CellTextInput({  }) {
+  return <> 
+    <div>
+    </div>
+  </>
+}
+
+function CellButtons({  }) {
+  return <> 
+    <div>
+    </div>
+  </>
+}
+
+function BoardCell({ idx }) {
+  return <> 
+    <div className="board-cell">
+      <CellHeader idx={idx} />
+      <CellImage />
+      <CellTextInput />
+      <CellButtons />
+    </div>
+  </>
+}
+
+function Boards({  }) {
+  const nRows = 3;
+  const nCols = 3;
+
+  const range = n => new Array(n).fill(null).map((_, i) => i);
+  const getIdx = (row, col) => row * nRows + col;
+
+  return <> 
+    <div className="boards">
+      <table>
+        {
+          range(nRows).map(row => (
+            <tr key={row}>
+              {
+                range(nCols).map(col => (
+                  <td key={col}>
+                    <BoardCell idx={getIdx(row, col)} />
+                  </td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </table>
+    </div>
   </>
 }
 
 function App() {
   return (
     <div className="App">
-      <Layout className="layout">
-        <Header>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          {/* <div className="site-layout-content">Content</div> */}
-          <Body />
-        </Content>
-        <Footer 
-          style={{ 
-            textAlign: 'center', 
-          }}
-        >
-          © Austin Poor 2021
-        </Footer>
-      </Layout>
+      <Header />
+      <Boards />
     </div>
   );
 }
